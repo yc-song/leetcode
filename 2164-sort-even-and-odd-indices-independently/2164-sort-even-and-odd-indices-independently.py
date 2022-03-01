cb0aa8e5fc2e28f1c08723cfe9da56b6ee34a43d
yc-song <hyeongoon@gmail.com>
2022-03-02 00:45:43 +0900

    reformatted by pep8

--- 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.py
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.py
@@ -12,11 +12,13 @@ class Solution:
     def find_minimum(self, l: list, start: int, end: int) -> int:
         min = start - 1
         for i in range(start, end):
-            if l[min] > l[i] and not i % 2: min = i
+            if l[min] > l[i] and not i % 2:
+                min = i
         return min
 
     def find_maximum(self, l: list, start: int, end: int) -> int:
         max = start - 1
         for i in range(start, end):
-            if l[max] < l[i] and i % 2: max = i
+            if l[max] < l[i] and i % 2:
+                max = i
         return max
