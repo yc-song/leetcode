0aa72dfe6e722ce89580d13a4e36c19d2e0b05b2
yc-song <57248081+yc-song@users.noreply.github.com>
2022-03-02 00:40:48 +0900

    Time: 180 ms (5.18%), Space: 14 MB (24.46%) - LeetHub

---
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.py
@@ -0,0 +1,22 @@
+class Solution:
+    def sortEvenOdd(self, nums: list[int]) -> list[int]:
+        for i in range(0, len(nums)):
+            if not i % 2:  # even
+                j = self.find_minimum(nums, i + 1, len(nums))
+                nums[i], nums[j] = nums[j], nums[i]
+            elif i % 2:
+                j = self.find_maximum(nums, i + 1, len(nums))
+                nums[i], nums[j] = nums[j], nums[i]
+        return nums
+
+    def find_minimum(self, l: list, start: int, end: int) -> int:
+        min = start - 1
+        for i in range(start, end):
+            if l[min] > l[i] and not i % 2: min = i
+        return min
+
+    def find_maximum(self, l: list, start: int, end: int) -> int:
+        max = start - 1
+        for i in range(start, end):
+            if l[max] < l[i] and i % 2: max = i
+        return max
