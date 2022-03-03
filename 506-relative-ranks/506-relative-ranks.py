2dd2455209f31961c9a9201684d4cfe6e3527942
yc-song <hyeongoon@gmail.com>
2022-03-03 17:35:20 +0900

    pep8 reformatted and add comment

--- 506-relative-ranks/506-relative-ranks.py
+++ 506-relative-ranks/506-relative-ranks.py
@@ -3,17 +3,17 @@ class Solution:
         idx_list = [None] * len(l)
         medal = [None] * len(l)
         for i in range(len(l)):  ## change to idx_list soon after
-            idx_list[i] = i
+            idx_list[i] = i # preserve index of input list
         self.__quicksorthelp(l, 0, len(l) - 1, idx_list)
         for j in range(len(l)):
             if j == 0:
-                medal[idx_list[j]] = "Gold Medal"
+                medal[idx_list[j]] = "Gold Medal" # idx_list[j] gives index of the first index of sorted list
             elif j == 1:
-                medal[idx_list[j]] = "Silver Medal"
+                medal[idx_list[j]] = "Silver Medal" # idx_list[j] gives index of the second index of sorted list
             elif j == 2:
-                medal[idx_list[j]] = "Bronze Medal"
+                medal[idx_list[j]] = "Bronze Medal" # idx_list[j] gives index of the third index of sorted list
             else:
-                medal[idx_list[j]] = str(j + 1)
+                medal[idx_list[j]] = str(j + 1) # idx_list[j] gives index of the 4th, 5th,... index of sorted list
         return medal
 
     def __quicksorthelp(self, l: list, start: int, end: int, idx_list: list) -> None:
@@ -31,7 +31,7 @@ class Solution:
             if l[i] > l[start]:
                 j += 1
                 l[j], l[i] = l[i], l[j]
-                idx_list[j], idx_list[i] = idx_list[i], idx_list[j]
+                idx_list[j], idx_list[i] = idx_list[i], idx_list[j] # index list is also swapped while list is swapped
         idx_list[start], idx_list[j] = idx_list[j], idx_list[start]
         l[start], l[j] = l[j], l[start]
         return j
