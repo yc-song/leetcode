e57c0b844afe9b6f174a33da1e58aaed0d9e335b
yc-song <57248081+yc-song@users.noreply.github.com>
2022-03-03 15:53:17 +0900

    Time: 331 ms (5.21%), Space: 14 MB (59.92%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.py
+++ 767-reorganize-string/767-reorganize-string.py
@@ -1,29 +1,26 @@
-from collections import deque
 class Solution:
     def reorganizeString(self, s: str) -> str:
+
         s = list(s)
-        s_sorted = deque()
-        counting_dict = dict()
+        s_sorted=[]
+        counting_dict=dict()
 
         for elem in s:
-            try:
-                counting_dict[elem] += 1
-            except KeyError:
-                counting_dict[elem] = 0
+            try: counting_dict[elem]+=1
+            except KeyError: counting_dict[elem]=0
 
         while counting_dict:
-            key = max(counting_dict, key=counting_dict.get)
-            for j in range(counting_dict[key] + 1):
+            key=max(counting_dict, key=counting_dict.get)
+            for j in range(counting_dict[key]+1):
                 s_sorted.append(key)
             del counting_dict[key]
 
-        max_count = s_sorted.count(s_sorted[0])
-        for i in range(len(s) - max_count):
+        max_count=s_sorted.count(s_sorted[0])
+        for i in range(len(s)-max_count):
             max_idx = [i for i, value in enumerate(s_sorted) if value == s_sorted[0]]
 
-            if s_sorted[max_count + i] != s_sorted[max_idx[0]]:
-                s_sorted.insert(max_idx[i % max_count] + 1, s_sorted[max_count + i])
-                del s_sorted[max_count+i+1]
-        for j in range(len(s_sorted) - 1):
-            if s_sorted[j] == s_sorted[j + 1]: return ""
+            if s_sorted[max_count+i]!=s_sorted[max_idx[0]]:
+                s_sorted.insert(max_idx[i%max_count]+1,s_sorted.pop(max_count+i))
+        for j in range(len(s_sorted)-1):
+            if s_sorted[j]==s_sorted[j+1]: return ""
         return ''.join(s_sorted)
