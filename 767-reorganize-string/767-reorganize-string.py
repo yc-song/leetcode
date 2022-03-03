0c13382fd8b880ce9e2619478a75af60e759d396
yc-song <57248081+yc-song@users.noreply.github.com>
2022-03-03 16:49:02 +0900

    Time: 140 ms (5.21%), Space: 13.9 MB (59.92%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.py
+++ 767-reorganize-string/767-reorganize-string.py
@@ -1,6 +1,5 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-
         s = list(s)
         counting_dict = dict()
 
@@ -14,18 +13,17 @@ class Solution:
             key = max(counting_dict, key=counting_dict.get)
             for j in range(0, counting_dict[key]):
                 s[loop+j]=key
-            # print(loop,j,s)
             loop+=counting_dict[key]
             del counting_dict[key]
         max_count = s.count(s[0])
-        for i in range(len(s) - max_count):
-            max_idx = [i for i, value in enumerate(s) if value == s[0]]
+        if loop-max_count<max_count-1: return ""
+        else:
+            for i in range(len(s) - max_count):
+                max_idx = [i for i, value in enumerate(s) if value == s[0]]
 
-            if s[max_count + i] != s[max_idx[0]]:
-                # print(max_idx, i%max_count, max_count+i)
+                if s[max_count + i] != s[max_idx[0]]:
 
-                s.insert(max_idx[i % max_count] + 1, s.pop(max_count + i))
-            # print(s)
-        for j in range(len(s) - 1):
-            if s[j] == s[j + 1]: return ""
-        return ''.join(s)
+                    s.insert(max_idx[i % max_count] + 1, s.pop(max_count + i))
+                # print(s)
+
+            return ''.join(s)
