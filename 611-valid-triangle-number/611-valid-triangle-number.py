edc9e7a2acce941f77fd61a20f218bd8d7c362cb
yc-song <57248081+yc-song@users.noreply.github.com>
2022-03-06 13:42:49 +0900

    Time: 3641 ms (20.81%), Space: 14 MB (84.20%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.py
+++ 611-valid-triangle-number/611-valid-triangle-number.py
@@ -12,21 +12,21 @@ class Solution:
                     ans+=idx-(j+1)
         return ans
 
-#     def binary_search(self, nums:list, start:int, end:int, target:int)->int:
-#         if start<end:
-#             mid=(start+end)//2
-#             # print(start,mid,end, target)
-#             if target>nums[mid]:
-#                 start=mid+1
-#                 return self.binary_search(nums, start, end, target)
-#             elif target==nums[mid]: return mid
-#             else:
-#                 end=mid-1
-#                 return self.binary_search(nums, start, end, target)
-#         elif target<=nums[start]:
-#             # print("1",start)
+    def binary_search(nums:list, start:int, end:int, target:int)->int:
+        if start<end:
+            mid=(start+end)//2
+            # print(start,mid,end, target)
+            if target>nums[mid]:
+                start=mid+1
+                return self.binary_search(nums, start, end, target)
+            else:
+                end=mid-1
+                return self.binary_search(nums, start, end, target)
+        elif target<=nums[start]:
+            # print("1",start)
 
-#             return start
+            return start
 
-#             # print("2")
-#         else: return start+1
+
+            # print("2")
+        else: return start+1
