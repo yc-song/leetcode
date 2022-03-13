bf5ade14839fb534722e490dcd4a35974fd360c9
yc-song <57248081+yc-song@users.noreply.github.com>
2022-03-13 20:46:37 +0900

    Time: 48 ms (71.08%), Space: 34.7 MB (67.86%) - LeetHub

---
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <vector>
+
+ 
+ class Solution {
+  public:
+      TreeNode* searchBST(TreeNode* root, int val) {
+          TreeNode* ans = new TreeNode;
+          if (root == nullptr) {
+              ans = nullptr;
+              return ans;
+          }
+          else {
+              if (root->val == val) {
+                 *ans = TreeNode(val, root->left, root->right);
+                  return ans;
+              }
+              else if (root->val < val) {
+                  return searchBST(root->right, val);
+              }
+              else if (root->val > val) {
+                  return searchBST(root->left, val);
+              }
+              return ans;
+          }
+      }
+  };
