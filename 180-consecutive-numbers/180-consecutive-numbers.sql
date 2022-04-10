087b3a8976346bc05d45f5b64525aaea7a43674e
yc-song <57248081+yc-song@users.noreply.github.com>
2022-04-10 17:06:04 +0900

    Time: 1503 ms (34.69%), Space: 0B (100.00%) - LeetHub

--- 180-consecutive-numbers/180-consecutive-numbers.sql
+++ 180-consecutive-numbers/180-consecutive-numbers.sql
@@ -1,10 +1,10 @@
 /* Write your T-SQL query statement below */
 select distinct logs.num as ConsecutiveNums
 from logs
-left outer join (select id, num 
+inner join (select id, num 
      from logs
      ) as l2 on (l2.id = logs.id+1)
-left outer join (select id, num 
+inner join (select id, num 
 from logs
 ) as l3 on (l3.id = logs.id+2)
 where logs.num = l2.num and l2.num = l3.num
