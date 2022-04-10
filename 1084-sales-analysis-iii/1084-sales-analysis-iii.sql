274122b20ce78a3e959ede80d709eaaafa95a943
yc-song <57248081+yc-song@users.noreply.github.com>
2022-04-10 16:40:18 +0900

    Time: 4401 ms (68.84%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,10 +1,9 @@
-/* Write your T-SQL query statement below */
-
 select distinct p.product_id, product_name
 from Product p
 join Sales s on (p.product_id= s.product_id)
-where sale_date>='2019-01-01' and sale_date<='2019-03-31' and
-p.product_id not in (select p.product_id
+where sale_date>='2019-01-01' and sale_date<='2019-03-31' 
+except
+select p.product_id, product_name
 from Product p
 join Sales s on (p.product_id= s.product_id)
-where (sale_date<'2019-01-01' or sale_date>'2019-03-31'))
+where (sale_date<'2019-01-01' or sale_date>'2019-03-31')
